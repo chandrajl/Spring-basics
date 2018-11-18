a9e46ce8b44d8a22c0261d24952c3c24cfb96d1f
Manyam <chandrasekhar.manyam@capgemini.com>
2018-11-18 12:47:36 -0600

    Spring profiles and logging

--- src/main/java/com/spring/learn/SpringBasicsApplication.java
+++ src/main/java/com/spring/learn/SpringBasicsApplication.java
@@ -2,7 +2,10 @@ package com.spring.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.spring.learn.properties.MyAppProperties;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.spring.learn")
@@ -12,7 +15,9 @@ public class SpringBasicsApplication {
 		//Enable or disable auto restart
 		System.setProperty("spring.devtools.restart.enabled", "true");
 		System.out.println("Hello World");
-		
-		SpringApplication.run(SpringBasicsApplication.class, args);
+
+		ConfigurableApplicationContext context=SpringApplication.run(SpringBasicsApplication.class, args);
+		MyAppProperties bean = context.getBean(MyAppProperties.class);
+		System.out.println(bean.getRefreshTimeUnit());
 	}
 }
--- src/main/java/com/spring/learn/controller/BasicsController.java
+++ src/main/java/com/spring/learn/controller/BasicsController.java
@@ -1,5 +1,8 @@
 package com.spring.learn.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicsController{
 
-	
+	Logger logger = LoggerFactory.getLogger(BasicsController.class);
+
 	@Value("${server.port}")
 	int port;
-	
+
 	@RequestMapping("/")
 	public String sayHello(){
+		logger.trace(String.valueOf(port));
+		logger.debug(String.valueOf(port));
+		logger.info(String.valueOf(port));
+		logger.warn(String.valueOf(port));
+		logger.error(String.valueOf(port));
 		return String.valueOf(port);
 	}
 }
